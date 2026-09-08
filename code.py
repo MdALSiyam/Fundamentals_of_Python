c5bc31182da9b14775252e9bbea9899a058d7f7c
Md. Abdul Latif Siyam <mdabdullatifsiyam87@gmail.com>
2026-09-08 22:43:29 +0600

    Initial Commit

---
+++ code.py
@@ -0,0 +1,26 @@
+#Output
+print("Hello Siyam")
+
+#Variables
+name = "Siyam"
+age = 25
+cgpa = 3.10
+isStudent = True
+
+print("My name is", name, "and I am", age, "years old.", "My CGPA is", cgpa)
+
+#Type of Variables
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(isStudent))
+
+#Input
+name = input("Enter your name: ")
+print("Hello", name)
+
+#Concatenation
+first_name = "Abdul"
+last_name = "Latif"
+full_name = first_name + " " + last_name
+print("Full name:", full_name)
