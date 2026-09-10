0c085ffe150aae4a73891454903bd05d88a38517
Md. Abdul Latif Siyam <mdabdullatifsiyam87@gmail.com>
2026-09-10 22:53:00 +0600

    Type Casting & Conversion Added

---
+++ part-2.py
@@ -0,0 +1,9 @@
+# Type Casting & Conversion
+age = input("Enter your age: ")
+new_age = int(age) + 1
+
+print(new_age)
+print(float(new_age))
+
+print(1 + 2.5) #Implicit Type Conversion
+print(1 + int(2.8)) #Explicit Type Conversion
